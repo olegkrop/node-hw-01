2a4e31fe3ba33df1e608e2115e47d9a2aa38c5b7
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-04-02 09:10:45 +0300

    initial

---
+++ contacts.js
@@ -0,0 +1,62 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db/contacts.json");
+
+const updateContacts = async (contacts) =>
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getContactById = async (id) => {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((item) => item.id === id);
+    return result || null;
+  } catch (error) {}
+};
+
+const addContact = async (data) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      ...data,
+    };
+    contacts.push(newContact);
+    await updateContacts(contacts);
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const removeContact = async (id) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === id);
+    if (index === -1) {
+      return null;
+    }
+    const [result] = contacts.splice(index, 1);
+    await updateContacts(contacts);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
---
+++ index.js
@@ -0,0 +1,80 @@
+// --------------------Start (using yargs)------------------------
+
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+    case "get":
+      const contact = await contacts.getContactById(id);
+      console.log(contact);
+      break;
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      console.log(deleteContact);
+      break;
+    default:
+      console.table("\x1B[31m Unknown action type!");
+  }
+};
+
+const arrey = hideBin(process.argv);
+const { argv } = yargs(arrey);
+
+invokeAction(argv);
+
+// --------------------End (using yargs)--------------------------
+
+// --------------------Start (using commander)--------------------
+
+// const { program } = require("commander");
+
+// const contacts = require("./contacts");
+
+// const invokeAction = async ({ action, id, name, email, phone }) => {
+//   switch (action) {
+//     case "list":
+//       const allContacts = await contacts.listContacts();
+//       console.table(allContacts);
+//       break;
+//     case "get":
+//       const contact = await contacts.getContactById(id);
+//       console.log(contact);
+//       break;
+//     case "add":
+//       const newContact = await contacts.addContact({ name, email, phone });
+//       console.log(newContact);
+//       break;
+//     case "remove":
+//       const deleteContact = await contacts.removeContact(id);
+//       console.log(deleteContact);
+//       break;
+//     default:
+//       console.table("\x1B[31m Unknown action type!");
+//   }
+// };
+
+// program
+//   .option("-a, --action <type>")
+//   .option("-i, --id <type>")
+//   .option("-n, --name <type>")
+//   .option("-e, --email <type>")
+//   .option("-p, --phone <type>");
+
+// program.parse();
+
+// const options = program.opts();
+// console.log(options);
+// invokeAction(options);
+
+// --------------------End (using commander)--------------------
